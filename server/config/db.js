5c69e0c91c3a3927dca376739a444081d8dd075c
Arpit kamal <arpit@Arpits-MacBook-Pro.local>
2021-02-25 12:07:51 +0530

    books added

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://arpit:1234@cluster0.hldao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
